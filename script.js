054efa657ce9c51b266f01de512b320c7c4e3841
Ad-Car <49081037+Ad-Car@users.noreply.github.com>
2024-11-26 21:34:44 +0000

    Add player getter and setter
    Increment and interrogate player's score by .increaseScore() and
    .getScore(), respectively

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-function Gameboard() {
-	const board = Array(9).fill(' ');
-	return board;
+function createGrid() {
+	let grid = Array(9).fill(' ');
+	return grid;
 }
 
-function Player(name, marker) {
-	return { name, marker };
+function createPlayer(name, marker) {
+	let score = 0;
+	const getScore = () => score;
+	const increaseScore = () => score++;
+	return { name, marker, getScore, increaseScore  };
 }
 
-function Play(player, square) {
-	if (gameboard[square] === ' ') {
-		gameboard[square] = player.marker;
+function play(player, square) {
+	if (grid[square] === ' ') {
+		grid[square] = player.marker;
 	}
 }
 
 function checkForWin(player) {
-	let row1 = gameboard.slice(0, 3);
-	let row2 = gameboard.slice(3, 6);
-	let row3 = gameboard.slice(6, 9);
-	let col1 = [ gameboard[0], gameboard[3], gameboard[6] ];
-	let col2 = [ gameboard[1], gameboard[4], gameboard[7] ];
-	let col3 = [ gameboard[2], gameboard[5], gameboard[8] ];
-	let diag1 = [ gameboard[0], gameboard[4], gameboard[8] ];
-	let diag2 = [ gameboard[2], gameboard[4], gameboard[6] ];
+	let row1 = grid.slice(0, 3);
+	let row2 = grid.slice(3, 6);
+	let row3 = grid.slice(6, 9);
+	let col1 = [ grid[0], grid[3], grid[6] ];
+	let col2 = [ grid[1], grid[4], grid[7] ];
+	let col3 = [ grid[2], grid[5], grid[8] ];
+	let diag1 = [ grid[0], grid[4], grid[8] ];
+	let diag2 = [ grid[2], grid[4], grid[6] ];
 	let checks = [row1, row2, row3, col1, col2, col3, diag1, diag2 ];
 	for (check of checks) {
 		let win = check.every( cell => cell === player.marker);
@@ -29,7 +32,7 @@ function checkForWin(player) {
 	} 
 }
 
-gameboard = Gameboard();
-const player1 = Player("Adam", "X");
-const player2 = Player("Bryn", "O");
+grid = createGrid();
+const player1 = createPlayer("Adam", "X");
+const player2 = createPlayer("Bryn", "O");
 
