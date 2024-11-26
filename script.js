73fc7a22e75132ed2b6eb48862afacce43ac3511
Ad-Car <49081037+Ad-Car@users.noreply.github.com>
2024-11-26 18:15:34 +0000

    Force gameboard to be a non-sparse array
    .every skips empty slots an array, which meant original version of
    check win function would return true on sub arrays containing even one
    counter. Fix this by calling .fill(' ') in the array constructor

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function Gameboard() {
-	const board = Array(9);
+	const board = Array(9).fill(' ');
 	return board;
 }
 
@@ -8,32 +8,28 @@ function Player(name, marker) {
 }
 
 function Play(player, square) {
-	if (!board[square] ) {
-		board[square] = player.marker;
+	if (gameboard[square] === ' ') {
+		gameboard[square] = player.marker;
 	}
 }
-function checkForWin(board, player) {
-	let row1 = board.slice(0, 3);
-	let row2 = board.slice(3, 6);
-	let row3 = board.slice(6, 9);
-	let col1 = [ board[0], board[3], board[6] ];
-	let col2 = [ board[1], board[4], board[7] ];
-	let col3 = [ board[2], board[5], board[8] ];
-	let diag1 = [ board[0], board[4], board[8] ];
-	let diag2 = [ board[2], board[4], board[6] ];
-	let rows = [row1, row2, row3, col1, col2, col3, diag1, diag2 ];
 
-	for (row of rows) {
-		let win = row.every( cell => cell === player.marker)
-	/*	console.log(player.name, player.marker, row, win)
-		*/if (win) { return true }
+function checkForWin(player) {
+	let row1 = gameboard.slice(0, 3);
+	let row2 = gameboard.slice(3, 6);
+	let row3 = gameboard.slice(6, 9);
+	let col1 = [ gameboard[0], gameboard[3], gameboard[6] ];
+	let col2 = [ gameboard[1], gameboard[4], gameboard[7] ];
+	let col3 = [ gameboard[2], gameboard[5], gameboard[8] ];
+	let diag1 = [ gameboard[0], gameboard[4], gameboard[8] ];
+	let diag2 = [ gameboard[2], gameboard[4], gameboard[6] ];
+	let checks = [row1, row2, row3, col1, col2, col3, diag1, diag2 ];
+	for (check of checks) {
+		let win = check.every( cell => cell === player.marker);
+		if (win) {return true };
 	} 
-	return false;
 }
 
-const board = Gameboard();
+gameboard = Gameboard();
 const player1 = Player("Adam", "X");
 const player2 = Player("Bryn", "O");
-
-
 
