1980f865afb45c9d40dd0fd745ace9f096491058
Ad-Car <49081037+Ad-Car@users.noreply.github.com>
2024-12-01 14:11:55 +0000

    Add colour-coding to scoreboard
    Active player's name is now displayed in green, non-active player's name,
    in red

--- script.js
+++ script.js
@@ -10,6 +10,94 @@ function createPlayer(name, marker) {
 	return { name, marker, getScore, increaseScore  };
 }
 
+function createScoreboard() {
+	let round = 1;
+	const getRound = () => round;
+	const nextRound = () => round++;
+	return { getRound, nextRound };
+}
+
+function createPlayerToggle() {
+	let currentPlayer = 'player1';
+
+	const getCurrentPlayer = () => {
+		return currentPlayer;
+	}
+	const togglePlayer = () => {
+		currentPlayer = (currentPlayer === 'player1') ? 'player2': 'player1';
+		displayActivePlayer(currentPlayer);	
+	}
+		return { getCurrentPlayer, togglePlayer }
+}
+
+function drawGrid() {
+	const existingCells = document.querySelectorAll(".cell");
+	if (existingCells ) {
+		existingCells.forEach( cell => {
+			cell.remove();
+		})
+	};
+
+	const gridContainer = document.querySelector(".grid-container");
+	for (i = 0; i < 9; i++) {
+		const cell = document.createElement("div");
+		gridContainer.appendChild(cell);
+		cell.setAttribute("id", `cell-${i}`);
+		cell.classList.add("cell");
+		document.getElementById(`cell-${i}`).textContent = grid[i];
+		cell.addEventListener("click", () => {
+			let cellNo = (cell.id).slice(-1);
+			placeMarker(cellNo);
+		});
+	}
+}
+
+function drawScoreboard() {
+	const scoreboardContainer = document.querySelector(".scoreboard-container");
+	for (i = 0; i < 3; i++) {
+		const holder = document.createElement("div");
+		scoreboardContainer.appendChild(holder);
+		holder.setAttribute("id", `scoreboard-holder-${i}`);
+		holder.classList.add("scoreboard-holder");
+	}
+}
+
+function displayActivePlayer(activePlayer) {
+	if ( activePlayer === 'player1' ) { 
+		document.querySelector('#scoreboard-holder-0').style.color = 'green'
+		document.querySelector('#scoreboard-holder-2').style.color = 'red'
+	} else {
+		document.querySelector('#scoreboard-holder-2').style.color = 'green'
+		document.querySelector('#scoreboard-holder-0').style.color = 'red'
+	} 
+}
+
+function writeToScoreboard() {
+	document.querySelector('#scoreboard-holder-0').textContent = `${player1.name} ${player1.getScore()}`
+	document.querySelector('#scoreboard-holder-1').textContent = `Round ${scoreboard.getRound()}`
+	document.querySelector('#scoreboard-holder-2').textContent = `${player2.name} ${player2.getScore()}`
+}
+
+function placeMarker(square) {
+	let activePlayer = game.getCurrentPlayer()
+	let marker = (activePlayer === 'player1') ? `${player1.marker}`: `${player2.marker}`;
+	if (grid[square] === ' ') {
+		grid[square] = marker;
+		drawGrid();
+		if(checkForWin(activePlayer)) {
+			console.log(`${activePlayer} WINS!`)
+			if(activePlayer === 'player1') { 
+				player1.increaseScore()
+			} else { player2.increaseScore() }
+			scoreboard.nextRound();
+			writeToScoreboard();
+			grid = createGrid();
+			drawGrid();
+		}
+		game.togglePlayer();
+	}
+}
+
 function play(player, square) {
 	if (grid[square] === ' ') {
 		grid[square] = player.marker;
@@ -27,12 +115,25 @@ function checkForWin(player) {
 	let diag2 = [ grid[2], grid[4], grid[6] ];
 	let checks = [row1, row2, row3, col1, col2, col3, diag1, diag2 ];
 	for (check of checks) {
-		let win = check.every( cell => cell === player.marker);
-		if (win) {return true };
+	let activePlayer = game.getCurrentPlayer()
+	let marker = (activePlayer === 'player1') ? `${player1.marker}`: `${player2.marker}`;
+		let win = check.every( cell => cell === marker);
+		if (win) { 
+			console.log(check)
+			return true };
 	} 
 }
 
 grid = createGrid();
+scoreboard = createScoreboard();
+
 const player1 = createPlayer("Adam", "X");
 const player2 = createPlayer("Bryn", "O");
+const game = createPlayerToggle();
 
+window.addEventListener('load', () => {
+	drawGrid();
+	drawScoreboard();
+	writeToScoreboard();
+	displayActivePlayer(game.getCurrentPlayer());
+});
